05a3e9e772071cb9019c1be9c7e7672085b6a4c2
full_name <email_address>
2021-06-14 09:24:49 -0400

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 
+const Search = ({ search }) => {
+  return (
+    <div>{search.name} {search.number}</div>
+  );
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '212-212-2122'}
+    { name: 'Arto Hellas', number: '212-212-2122' }, 
+    { name: 'Ada Lovelace', number: '39-44-5323523' }, 
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
   const peopleList = persons.map((person) => 
     <div key={person.name}>
@@ -39,9 +49,28 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  }
+
+  const searchResults = search ? persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())) : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with 
+          <input
+            value={search}
+            onChange={handleSearchChange}
+          />
+      </div>
+      <ul>
+          {searchResults.map(search => 
+           <Search key={search.name} search={search} />)}
+      </ul>
+      <h2>add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: 
